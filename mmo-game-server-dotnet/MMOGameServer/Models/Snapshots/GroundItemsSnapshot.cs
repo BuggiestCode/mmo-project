20cfcf0c2b055f62cc2e6ea8d549900851528cde
JackSainsbury <JackSainsbury@hotmail.co.uk>
2025-09-07 10:59:20 -0400

    change GroundItem to be iEquatable to do an == operator overload for the UID comparison

--- mmo-game-server-dotnet/MMOGameServer/Models/Snapshots/GroundItemsSnapshot.cs
+++ mmo-game-server-dotnet/MMOGameServer/Models/Snapshots/GroundItemsSnapshot.cs
@@ -6,12 +6,28 @@ namespace MMOGameServer.Models.Snapshots;
 /// Represents a single item in a stack on the ground 
 /// (uniquely identifyable and non unique type identifiable)
 /// </summary>
-public class GroundItem
+public class GroundItem : IEquatable<GroundItem>
 {
     [JsonPropertyName("instanceID")]
-    public int InstanceID;
+    public int InstanceID { get; set; }
     [JsonPropertyName("itemID")]
-    public int ItemID;
+    public int ItemID { get; set; }
+    
+    public bool Equals(GroundItem? other)
+    {
+        if (other is null) return false;
+        return InstanceID == other.InstanceID;
+    }
+    
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as GroundItem);
+    }
+    
+    public override int GetHashCode()
+    {
+        return InstanceID.GetHashCode();
+    }
 }
 
 /// <summary>
